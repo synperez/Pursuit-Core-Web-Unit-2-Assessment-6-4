d9fcc7f6055404271078148d350a1486dd1d9a9a
synperez <synperez@Syns-Macbook-Pro.local>
2019-12-19 11:47:42 -0500

    id grabs, api grab

--- a2.js
+++ a2.js
@@ -1,4 +1,24 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    console.log("pie");
-    
-})
+document.addEventListener('DOMContentLoaded', ()=>{
+    let select = document.querySelector("#select")
+    let movieName = document.querySelector("#movieName")
+    let movieDate = document.querySelector("#movieDate")
+    let moviePlot = document.querySelector("#moviePlot")
+    let text = document.querySelector("#text")
+    let submit = document.querySelector("#submit")
+    let review = document.querySelector("#review")
+  
+    const getMovie = async() =>{
+      try{
+          let res = await axios.get("https://ghibliapi.herokuapp.com/films")
+          let title = res.data
+          title.forEach(movie=>{
+              let option = document.createElement("option")
+              option.innerText = movie.title
+              select.appendChild(option)
+          })
+      }catch(error){
+          console.log(error)
+    }
+  }
+    getMovie()
+  })
